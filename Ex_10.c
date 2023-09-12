b53b6a42007953e61dd545669a84f49e5eabb4d6
Jeferson <jefersod@metropolia.fi>
2023-09-12 15:55:07 +0300

    Exercise 10

---
+++ Ex_10.c
@@ -0,0 +1,21 @@
+//
+// Created by Jeferson on 11/09/2023.
+//
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char word[32];
+    do {
+        printf("Enter a string. or type 'stop' to exit: ");
+        fgets(word, sizeof(word), stdin);
+        //printf("%s", word)
+        if (word[strlen(word) - 1] == '\n') {
+            word[strlen(word) - 1] = '\0';
+        }
+        printf("The amount of characters in '%s' is: %llu\n", word, strlen(word));
+    }while (strcmp(word, "stop") != 0);
+
+    return 0;
+}
