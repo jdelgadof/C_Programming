216769ee2d3908e96d4a3641d68985f9c2699342
Jeferson <jefersod@metropolia.fi>
2023-10-02 23:22:38 +0300

    Exercise 18

--- Ex_18.c
+++ Ex_18.c
@@ -7,18 +7,17 @@
 
 int main(void) {
     int number;
-
     srand(time(NULL));
     do {
-        printf("Please enter a number between 0-15 or a negative number to quit");
+        printf("Please enter a number between 0-15 or a negative number to quit: ");
         scanf("%d", &number);
         if (number >= 0 && number <= 15) {
             int rand_int = rand() & 0xFFFF;
-            printf("Original number: [0x%X]\n", rand_int);
+            printf("Original random number: \t\t\t[0x%X]\n", rand_int);
             rand_int = rand_int >> number;
-            printf("After moving %d bits to the right [0x%X]\n", number, rand_int);
-            rand_int = rand_int & 0x21;          //Clears all bit but 0 and 5 [0010 0001]
-            printf("After clearing everything but bit 0 and 5 [0x%02X]\n", rand_int);
+            printf("After moving %d bits to the right: \t\t[0x%X]\n", number, rand_int);
+            rand_int = rand_int & 0x3F;          //Clears all bit but 0 to 5 [0011 1111]
+            printf("After clearing everything but bits 0 to 5: \t[0x%02X]\n", rand_int);
         } else if(number>15){
             printf("Invalid input\n");
         }
